dc3dd0c7a8074db4f97530307e5e6def27defac5
Courtney Lee <111788457+c-mei-l@users.noreply.github.com>
2023-07-19 20:37:04 -0500

    Add files via upload

---
+++ Project 2/hellolanguage.hpp
@@ -0,0 +1,13 @@
+//
+//  hellolanguage.hpp
+//  
+//
+//  Created by Courtney Lee on 8/19/22.
+//
+
+#ifndef hellolanguage_hpp
+#define hellolanguage_hpp
+
+#include <stdio.h>
+
+#endif /* hellolanguage_hpp */
